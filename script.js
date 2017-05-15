56c528806a34ebd17fce8679c3551ca28ba54a45
zion Niq <zion Niq>
2017-05-15 15:07:43 -0400

    realized onclick function tags in html file can pass in arguments, this simplifies a lot of unnec code in script.js

--- script.js
+++ script.js
@@ -1,60 +1,7 @@
 $('#debug').html('started loading script.js');
 
-function b1(){
-	switchAdjBlocks(1);
-}
-function b2(){
-	switchAdjBlocks(2);
-}
-function b3(){
-	switchAdjBlocks(3);
-}
-function b4(){
-	switchAdjBlocks(4);
-}
-function b5(){
-	switchAdjBlocks(5);
-}
-function b6(){
-	switchAdjBlocks(6);
-}
-function b7(){
-	switchAdjBlocks(7);
-}
-function b8(){
-	switchAdjBlocks(8);
-}
-function b0(){
-	$('#debug').html('clicked img0');
-	switchAdjBlocks(0);
-}
-
-function db0(){
-	$('#blockImgInfo').html(blocks[0]);
-}
-function db1(){
-	$('#blockImgInfo').html(blocks[1]);
-}
-function db2(){
-	$('#blockImgInfo').html(blocks[0]);
-}
-function db3(){
-	$('#blockImgInfo').html(blocks[0]);
-}
-function db4(){
-	$('#blockImgInfo').html(blocks[0]);
-}
-function db5(){
-	$('#blockImgInfo').html(blocks[0]);
-}
-function db6(){
-	$('#blockImgInfo').html(blocks[0]);
-}
-function db7(){
-	$('#blockImgInfo').html(blocks[0]);
-}
-function db8(){
-	$('#blockImgInfo').html(blocks[0]);
+function mousedOver(n){
+	$('#blockImgInfo').html(blocks[n].toString());
 }
 
 function toggleDebugP(){
